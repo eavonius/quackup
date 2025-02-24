06425363857817f9c8f3f778ced63864d5de21c8
eavonius <jayme.edwards@gmail.com>
2025-02-23 20:52:11 -0600

    fix: Initial release to pypi.

--- quackup/config.py
+++ quackup/config.py
@@ -1,11 +1,15 @@
-import os
+"""Configuration for quackup."""
+
 import configparser
+import os
+
 from dotenv import load_dotenv
 
 DEFAULT_CONFIG = """
 [quackup]
 db_env_var = DUCKDB_PATH
 """
+
 
 def get_db_path() -> str:
     """Retrieve the path to the duckdb database."""
@@ -14,17 +18,20 @@ def get_db_path() -> str:
     load_dotenv()
 
     # Load configuration from quackup.ini
-    CONFIG_FILE = 'quackup.ini'
+    CONFIG_FILE = "quackup.ini"
     config = configparser.ConfigParser()
     config.read(CONFIG_FILE)
 
     # Get the environment variable name for the DuckDB file path
-    db_env_var = config.get('quackup', 'db_env_var', fallback='DUCKDB_PATH')
+    db_env_var = config.get("quackup", "db_env_var", fallback="DUCKDB_PATH")
 
     # Get the actual DuckDB file path from the environment
     db_path = os.getenv(db_env_var)
 
     if not db_path:
-        raise ValueError(f"Environment variable '{db_env_var}' is not set or does not contain a valid path.")
+        raise ValueError(
+            f"Environment variable '{db_env_var}' is not set "
+            "or does not contain a valid path."
+        )
 
     return db_path
